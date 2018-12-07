4fed656d527fccaaa62859a7337b6c338dbee2b4
Cornel Viorel <viorelporumbescu@outlook.com>
2018-12-07 10:52:03 +0200

    Initial Commit

---
+++ AdventOfCode2018/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AdventOfCode2018
+//
+//  Created by Viorel Porumbescu on 07/12/2018.
+//  Copyright © 2018 Viorel Porumbescu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
